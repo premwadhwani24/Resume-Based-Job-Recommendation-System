881e500ba506e45a9efafb83f791a3cd38ccdb7c
premwadhwani24 <premwadhwani24@gmail.com>
2025-04-12 23:38:34 +0530

    initial commit

---
+++ src/main/java/io/github/premwadhwani24/resumeanalyzer/resumeanalyzer/ResumeBasedJobRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.premwadhwani24.resumeanalyzer.resumeanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeBasedJobRecommendationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeBasedJobRecommendationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/premwadhwani24/resumeanalyzer/resumeanalyzer/ResumeBasedJobRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.premwadhwani24.resumeanalyzer.resumeanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBasedJobRecommendationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
