ff1114fd20ce7ea0957d249dfea6f25c7bb3a776
Priyanshu bhardwaj <126265344+Priyanshu04-git@users.noreply.github.com>
2025-04-13 12:33:02 +0530

    POJO class

--- src/main/java/io/github/premwadhwani24/resumeanalyzer/resumeanalyzer/ResumeBasedJobRecommendationSystemApplication.java
+++ src/main/java/io/github/premwadhwani24/resumeanalyzer/resumeanalyzer/ResumeBasedJobRecommendationSystemApplication.java
@@ -3,11 +3,12 @@ package io.github.premwadhwani24.resumeanalyzer.resumeanalyzer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Main Spring Boot application entry point
 @SpringBootApplication
 public class ResumeBasedJobRecommendationSystemApplication {
 
+    // Application entry point (standard Spring Boot application launcher)
     public static void main(String[] args) {
         SpringApplication.run(ResumeBasedJobRecommendationSystemApplication.class, args);
     }
-
 }
